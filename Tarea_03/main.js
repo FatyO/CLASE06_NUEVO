2304c24e4c5549cd2b9fbab41c489c3cca01b963
Fatima Oviedo <foviedo@centro.edu.mx>
2024-10-10 16:04:46 -0600

    Tareas 3

--- Tarea_03/main.js
+++ Tarea_03/main.js
@@ -3,15 +3,37 @@ window.onload = function() {
     const ctx = canvas.getContext('2d');
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
-    const mainCircleRadius = 250;
+    const mainCircleRadius = 250; 
     const speedFactor = 0.2; 
     let circles = [];
 
-    
+   
     for (let i = 0; i < 10; i++) {
-        let radius = Math.random() * 20 + 10; 
-        let angle = Math.random() * Math.PI * 2; 
-        let distance = Math.random() * (mainCircleRadius - radius); 
+        
+        let radius, angle, distance;
+
+        
+        if (Math.random() > 0.5) {
+            radius = Math.random() * 20 + 10; 
+        } else {
+            radius = 15;  
+        }
+
+       
+        if (Math.random() > 0.5) {
+            angle = Math.random() * Math.PI * 2;  
+        } else {
+            angle = Math.PI; 
+        }
+
+        
+        if (Math.random() > 0.5) {
+            distance = Math.random() * (mainCircleRadius - radius);  
+        } else {
+            distance = mainCircleRadius / 2; 
+        }
+
+        
         circles.push({
             radius: radius,
             x: centerX + Math.cos(angle) * distance,
@@ -26,35 +48,29 @@ window.onload = function() {
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        
         ctx.beginPath();
         ctx.arc(centerX, centerY, mainCircleRadius, 0, Math.PI * 2);
         ctx.strokeStyle = "rgba(0, 150, 255, 0.5)";
         ctx.lineWidth = 3;
         ctx.stroke();
 
-        
         circles.forEach(circle => {
-            
             if (circle.distance + circle.radius > mainCircleRadius) {
                 circle.growing = false;
             } else if (circle.distance - circle.radius < 0) {
                 circle.growing = true;
             }
 
-            
             if (circle.growing) {
-                circle.radius += 0.1 * speedFactor;  
+                circle.radius += 0.1 * speedFactor; 
             } else {
                 circle.radius -= 0.1 * speedFactor;
             }
 
-            
             circle.angle += circle.speed * speedFactor;
             circle.x = centerX + Math.cos(circle.angle) * circle.distance;
             circle.y = centerY + Math.sin(circle.angle) * circle.distance;
 
-            
             ctx.beginPath();
             ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
             ctx.fillStyle = "rgba(0, 150, 255, 0.5)";
